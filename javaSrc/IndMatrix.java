53525ef602fac1455aa10ba5e0e89b0f8bfa1fa0
allencho <s3558745@student.rmit.edu.au>
2018-03-23 14:46:30 +1100

    Commenting for methods description

--- javaSrc/IndMatrix.java
+++ javaSrc/IndMatrix.java
@@ -17,19 +17,25 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 	 */
     public IndMatrix() {
     	// Implement me!
+    		int vertex;
+    		int edge;
+    		int[][] incidenceMatrix;
     } // end of IndMatrix()
     
-    
+	// add vertices
     public void addVertex(T vertLabel) {
         // Implement me!
     } // end of addVertex()
 	
     
+    // srcLabel: starting point
+    // tarLabel: target point
+    // this method is to add connect vertices
     public void addEdge(T srcLabel, T tarLabel) {
         // Implement me!
     } // end of addEdge()
 	
-
+	// Display every vertex connected with source vertex
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
         
@@ -38,22 +44,22 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
         return neighbours;
     } // end of neighbours()
     
-    
+    // remove a vertex as the name describes
     public void removeVertex(T vertLabel) {
         // Implement me!
     } // end of removeVertex()
 	
-    
+    // remove edge b/w vertices
     public void removeEdge(T srcLabel, T tarLabel) {
         // Implement me!
     } // end of removeEdges()
 	
-    
+    // print current vertices
     public void printVertices(PrintWriter os) {
         // Implement me!
     } // end of printVertices()
 	
-    
+    // print current edges connecting vertices
     public void printEdges(PrintWriter os) {
         // Implement me!
     } // end of printEdges()
