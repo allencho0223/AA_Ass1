aed1f4dd91d42df289f755dc2f57267dd43a2f77
allencho <s3558745@student.rmit.edu.au>
2018-04-02 17:10:22 +1000

    Revert "Working on addVertex"
    This reverts commit 9ea80786aecf350419d487c686b5bf379dddc75c.

--- javaSrc/IndMatrix.java
+++ javaSrc/IndMatrix.java
@@ -12,16 +12,12 @@ import java.util.*;
 public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 {
 	
-    int vertNum = 1;
+    int vertNum = 0;
     @SuppressWarnings("unchecked")
     T edgeDisconnected = (T) "0";
-    @SuppressWarnings("unchecked")
     T edgeConnected = (T) "1";
-    private int edgeNum = 1;
-    private int maxEdgeNum = 0;
-    private int maxVertNum = 0;
-    @SuppressWarnings("unchecked")
-    T[] vertIndex = (T[])new Object[1];
+    private int nextEdge = 0;
+    private T[] vertIndex;
 	
 	
 	private T[][] incMatrix;
@@ -33,21 +29,16 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 	@SuppressWarnings("unchecked")
     public IndMatrix() {
     	// Implement me!
-	    // Initial array setting
-	    /*
-	     * 0 e1 e2 e3 e4 e5 e6         a[0][0] a[0][1] a[0][2] .....
-	     * A 0  0  0  0  0  0          a[1][0] a[1][1] a[1][2] .....
-	     * B 0  0  0  0  0  0      =>  a[2][0] a[2][1] a[2][2] .....
-	     * C 0  0  0  0  0  0          a[3][0] a[3][1] a[3][2] .....
-	     * 
-	     */
-    		incMatrix = (T[][]) new Object[1][1];
+    		incMatrix = (T[][]) new Object[0][0];
     } // end of IndMatrix()
     
 	// add vertices
     @SuppressWarnings("unchecked")
     public void addVertex(T vertLabel) {
         // Implement me!
+        
+        int matrixLength = incMatrix.length;
+        
         // Check if the vertex exists in current matrix
         for (T vertex : vertices) {
             if (vertex.equals(vertLabel)) {
@@ -56,65 +47,36 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
             }
         }
         
-     
-        // When vert 1 -> edge 1, array[2][2]
-        // When vert 2 -> edge 3, array[3][4] 
-        // When vert 3 -> edge 6, array[4][7]
-        // When vert 4 -> edge 10, array[5][11]
+//        if (vertNum == matrixLength) {
+        matrixLength++;
+        T[][] newIncMatrix = (T[][]) new Object[matrixLength][matrixLength];
         
-        // Calculate the maximum number of edges
-        maxEdgeNum = vertNum * (vertNum + 1) / 2;
-        
-        // create a new matrix and temp matrix to transfer data
-        incMatrix = (T[][]) new Object[vertNum + 1][maxEdgeNum + 1];
-        
-        T[][] newIncMatrix = (T[][]) new Object[vertNum + 1][maxEdgeNum + 1];
-        
-        for (int i = 0; i < vertNum; i++) {
-            for (int j = 0; j < maxEdgeNum; j++) {
+        for (int i = 0; i < incMatrix.length; i++) {
+            for (int j = 0; j < incMatrix[i].length; j++) {
                 newIncMatrix[i][j] = incMatrix[i][j];
             }
         }
-        
         incMatrix = newIncMatrix;
+//        }
         
         vertices.add(vertLabel);
         
-     // Store vertices into the row and column 0 based on the number of vertices
-        vertIndex[0] = vertLabel;
-        incMatrix[vertNum][0] = (T) vertIndex;
-        
-        
-//        for (int vertex = vertNum; vertex >= 1; vertex--) {
-//            vertIndex = (T[])new Object[1];
-//            vertIndex[0] = vertLabel;
-//            incMatrix[vertex][0] = (T) vertIndex;
-//        }
-        
-        // Store edges (initially make it 0) into the column (maximum number of columns to avoid array out of bounds exception) 
-        for (int vertex = vertNum; vertex >= 1; vertex--) {
-            for (int edge = maxEdgeNum; edge >= 1; edge--) {
-                vertIndex = (T[])new Object[1];
-                vertIndex[0] = edgeDisconnected;
-                incMatrix[vertex][edge] = (T) vertIndex;
-            }
+        // Store vertex, edge into vertIndex array then assign to the matrix at the end
+        for (int row = vertNum; row >= 0; row--) {
+            vertIndex = (T[])new Object[3];
+            vertIndex[0] = vertices.get(row);
+            vertIndex[1] = vertLabel;
+            vertIndex[2] = edgeDisconnected;
+            incMatrix[row][vertNum] = (T) vertIndex; 
         }
         
-//        for (int row = vertNum; row >= 0; row--) {
-//            vertIndex = (T[])new Object[3];
-//            vertIndex[0] = vertices.get(row);
-//            vertIndex[1] = vertLabel;
-//            vertIndex[2] = edgeDisconnected;
-//            incMatrix[row][0] = (T) vertIndex; 
-//        }
-//        
-//        for (int col = vertNum; col >= 0; col--) {
-//            vertIndex = (T[])new Object[3];
-//            vertIndex[0] = vertLabel;
-//            vertIndex[1] = vertices.get(col);
-//            vertIndex[2] = edgeDisconnected;
-//            incMatrix[vertNum][col] = (T) vertIndex;
-//        }
+        for (int col = vertNum; col >= 0; col--) {
+            vertIndex = (T[])new Object[3];
+            vertIndex[0] = vertLabel;
+            vertIndex[1] = vertices.get(col);
+            vertIndex[2] = edgeDisconnected;
+            incMatrix[vertNum][col] = (T) vertIndex;
+        }
         
         
         vertNum++;
@@ -126,37 +88,18 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
         int srcIndex = 0;
         int tarIndex = 0;
         
-        T[] edgeArray = (T[]) new Object[1];
+        T[] edgeArray = (T[]) new Object[3];
         
     		try {
-    		    if (incMatrix.length >= 2 && (vertices.contains(srcLabel) && vertices.contains(tarLabel))) {
-    		        
-    		        srcIndex = vertices.indexOf(srcLabel);
-                tarIndex = vertices.indexOf(tarLabel);
-    		        
-    		        edgeArray = (T[]) incMatrix[maxVertNum][maxEdgeNum];
-    		        
-    		        System.out.println("src: " + srcIndex);
-    		        System.out.println("tar: " + tarIndex);
-//    		        for (T vertex : vertices) {
-//    		            if (vertex.equals(srcLabel)) {
-//    		                System.out.println("the vertex already exists");
-//    		                return;
-//    		            }
-//    		        }
-    		        
-    		        
-//    		       srcIndex = vertices.indexOf(srcLabel);
-//    		       tarIndex = vertices.indexOf(tarLabel);
-//
-//    		       edgeArray = (T[]) incMatrix[srcIndex][tarIndex];
-//    		       if (edgeArray[2] == edgeDisconnected) {
-//    		           edgeArray[2] = edgeConnected;
-//    		       }
-//    		       edgeArray = (T[]) incMatrix[tarIndex][srcIndex];
-//    		       if (edgeArray[2] == edgeDisconnected) {
-//                   edgeArray[2] = edgeConnected;
-//               }
+    		    if (incMatrix.length >= 1 && (vertices.contains(srcLabel) && vertices.contains(tarLabel))) {
+    		       srcIndex = vertices.indexOf(srcLabel);
+    		       tarIndex = vertices.indexOf(tarLabel);
+
+    		       edgeArray = (T[]) incMatrix[srcIndex][tarIndex];
+    		       if (edgeArray[2] == edgeDisconnected) {
+    		           edgeArray[2] = edgeConnected;
+    		       }
+    		       nextEdge++;
     		        
     		    } else {
     		        System.err.println("Not enough number of vertices to be connected");
