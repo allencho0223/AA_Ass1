33808dd57f745680644018d35610cde3b2ce5fe1
rmit-s3491303-Christina-Papadopoulos <s3491303@student.rmit.edu.au>
2018-04-03 20:45:08 +1000

    Add files via upload

---
+++ javaSrc/Edge.java
@@ -0,0 +1,34 @@
+
+public class Edge  <T extends Object> {
+
+private T vertex1;
+private T vertex2;
+	
+public Edge(T vertex1, T vertex2) 
+{
+	this.vertex1 = vertex1;
+	this.vertex2 = vertex2;
+}
+
+public T getVertex1() {
+	return vertex1;
+}
+
+public void setVertex1(T vertex1) {
+	this.vertex1 = vertex1;
+}
+
+public T getVertex2() {
+	return vertex2;
+}
+
+public void setVertex2(T vertex2) {
+	this.vertex2 = vertex2;
+}
+	
+public String toString() {
+	
+	return String.format("%s/%s", vertex1, vertex2);
+	}
+
+}
--- javaSrc/IndMatrix.java
+++ javaSrc/IndMatrix.java
@@ -11,175 +11,104 @@ import java.util.*;
  */
 public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 {
-	
-    int vertNum = 0;
-    @SuppressWarnings("unchecked")
-    T edgeDisconnected = (T) "0";
-    T edgeConnected = (T) "1";
-    private int nextEdge = 0;
-    private T[] vertIndex;
-	
-	
-	private T[][] incMatrix;
-	List<T> vertices = new ArrayList<T>();
-	
-	/**
-	 * Contructs empty graph.
-	 */
-	@SuppressWarnings("unchecked")
+	private T[][] indMatrix;
+	List<T> vertexList = new ArrayList<T>();
+	List<T> edgeList = new ArrayList<T>();
+	List<Edge> edgeCollection = new ArrayList<Edge>();
+	private T[] matrixArray;
+	Edge edge;
+	T e = (T) "0";
+
     public IndMatrix() {
-    	// Implement me!
-    		incMatrix = (T[][]) new Object[0][0];
+    	indMatrix = (T[][])new Object[12][15];
+
     } // end of IndMatrix()
     
-	// add vertices
+    
     @SuppressWarnings("unchecked")
-    public void addVertex(T vertLabel) {
-        // Implement me!
-        
-        int matrixLength = incMatrix.length;
-        
-        // Check if the vertex exists in current matrix
-        for (T vertex : vertices) {
-            if (vertex.equals(vertLabel)) {
-                System.out.println("the vertex already exists");
-                return;
-            }
-        }
-        
-//        if (vertNum == matrixLength) {
-        matrixLength++;
-        T[][] newIncMatrix = (T[][]) new Object[matrixLength][matrixLength];
-        
-        for (int i = 0; i < incMatrix.length; i++) {
-            for (int j = 0; j < incMatrix[i].length; j++) {
-                newIncMatrix[i][j] = incMatrix[i][j];
-            }
-        }
-        incMatrix = newIncMatrix;
-//        }
-        
-        vertices.add(vertLabel);
-        
-        // Store vertex, edge into vertIndex array then assign to the matrix at the end
-        for (int row = vertNum; row >= 0; row--) {
-            vertIndex = (T[])new Object[3];
-            vertIndex[0] = vertices.get(row);
-            vertIndex[1] = vertLabel;
-            vertIndex[2] = edgeDisconnected;
-            incMatrix[row][vertNum] = (T) vertIndex; 
-        }
-        
-        for (int col = vertNum; col >= 0; col--) {
-            vertIndex = (T[])new Object[3];
-            vertIndex[0] = vertLabel;
-            vertIndex[1] = vertices.get(col);
-            vertIndex[2] = edgeDisconnected;
-            incMatrix[vertNum][col] = (T) vertIndex;
-        }
-        
-        
-        vertNum++;
+	public void addVertex(T vertLabel) {
+    	//int matrixLength = indMatrix.length;
+    	
+    	if (addtoVertexList(vertLabel) == true) 
+    		vertexList.add(vertLabel);
+    	//System.out.println("vertexlist = " + vertexList);
+    	
+    	edgeList.add(vertLabel); 
+    	if (edgeList.size() > 1 && edgeList.size() % 2 == 0) {
+    	for (int i = 0; i < edgeList.size() -1 ; i++ ) 
+    	{
+    		T vertex1 = edgeList.get(i);
+    		int next = ++i;
+    		T vertex2 = edgeList.get(next);
+    		edge = new Edge(vertex1, vertex2);
+
+    	}
+    	edgeCollection.add(edge); 
+    	//System.out.println("edgeCollection = " + edge.toString());
+    	}  
+    	
+    	
+    	for (int row = 0; row<vertexList.size(); row++) 
+    	{
+    		for (int col = 0; col < edgeCollection.size(); col++) 
+    		{
+    			matrixArray = (T[])new Object[3];
+    			matrixArray[0] = vertexList.get(row);
+    			if (edgeList.size() > 1)
+    			matrixArray[1] = (T) edgeCollection.get(col);
+    			else 
+    				matrixArray[1] = null;
+    			matrixArray[2] = e;
+    			indMatrix[row][col] = (T) matrixArray; 
+    		}
+    	}
+    	
+    	
+    	
+    	
     } // end of addVertex()
 	
-    @SuppressWarnings("unchecked")
+    
     public void addEdge(T srcLabel, T tarLabel) {
         // Implement me!
-        int srcIndex = 0;
-        int tarIndex = 0;
-        
-        T[] edgeArray = (T[]) new Object[3];
-        
-    		try {
-    		    if (incMatrix.length >= 1 && (vertices.contains(srcLabel) && vertices.contains(tarLabel))) {
-    		       srcIndex = vertices.indexOf(srcLabel);
-    		       tarIndex = vertices.indexOf(tarLabel);
-
-    		       edgeArray = (T[]) incMatrix[srcIndex][tarIndex];
-    		       if (edgeArray[2] == edgeDisconnected) {
-    		           edgeArray[2] = edgeConnected;
-    		       }
-    		       nextEdge++;
-    		        
-    		    } else {
-    		        System.err.println("Not enough number of vertices to be connected");
-    		    }
-    			
-    		} catch (NullPointerException npe) {
-    			System.err.println(npe.getLocalizedMessage());
-    		} catch (ArrayIndexOutOfBoundsException ai) {
-    		    System.err.println(ai.getLocalizedMessage());
-    		}
     } // end of addEdge()
 	
-	// Display every vertex connected with source vertex
+
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
-//        neighbours.contains(vertLabel);
+        
         // Implement me!
         
         return neighbours;
     } // end of neighbours()
     
-    // remove a vertex as the name describes
+    
     public void removeVertex(T vertLabel) {
         // Implement me!
     } // end of removeVertex()
 	
-    // remove edge b/w vertices
+    
     public void removeEdge(T srcLabel, T tarLabel) {
         // Implement me!
     } // end of removeEdges()
 	
-    // print current vertices
+    
     public void printVertices(PrintWriter os) {
-        
-        for (int i = 0; i < incMatrix.length; i++) {
-            for (int j = 0; j < incMatrix[i].length; j++) {
-                System.out.print(Arrays.deepToString((Object[]) incMatrix[i][j]));      
+    	for (int i = 0; i < indMatrix.length; i++) {
+    	    for (int j = 0; j < indMatrix[i].length; j++) {
+    	    	System.out.print(Arrays.deepToString((Object[]) indMatrix[i][j]));    	
 
-            }System.out.println(" ");
-        
-        }
-        
-        try {
-            for (T v: vertices) {
-                os.print(v + " "); 
-                }
-        } finally {
-            if (os != null) {
-                os.close();
-            }
-        }
+    	    }System.out.println(" ");
+    	
+    	}
+    }
+	
     
-    } // end of printVertices()
-    
-    
-    @SuppressWarnings("unchecked")
     public void printEdges(PrintWriter os) {
-        T[] edge = null;
-        try {
-            for (int i = 0; i < incMatrix.length; i++)
-            {
-                for (int j = 0; j < incMatrix[i].length; j++) 
-                {
-                    edge = (T[]) incMatrix[i][j];
-                    if (edge[2].equals("1"))
-                    os.println(edge[0] + " " + edge[1]);
-                }
-            }       
-            
-        } finally {
-            if (os != null) {
-                os.close();
-            }
-        }
-
+        // Implement me!
     } // end of printEdges()
     
-    // displays the selected vertices and distance value
-    // e.g. S A E returns A E 3 like this
-    // if the 2 vertices are not connected, it returns -1
+    
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
     	// Implement me!
     	
@@ -187,4 +116,21 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
         return disconnectedDist;    	
     } // end of shortestPathDistance()
     
+    
+    
+    public boolean addtoVertexList(T vertLabel)
+    {
+    	if (vertexList.contains(vertLabel)){
+    		return false;
+    	}
+    	return true;
+    }
+    
 } // end of class IndMatrix
+
+
+
+
+
+
+
