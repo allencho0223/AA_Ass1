ef48be5b6f89df75be67e378508a02d998bc2525
allencho <s3558745@student.rmit.edu.au>
2018-04-07 08:23:40 +1000

    Retrieve data from text file
    Now might be able to access a specific data for further process

--- javaSrc/DataGenerator.java
+++ javaSrc/DataGenerator.java
@@ -1,39 +1,62 @@
 import java.io.*;
+import java.util.*;
 
 public class DataGenerator {
+    
+    protected static final String progName = "DataGenerator";
+    protected String[] dataSet = null;
+    protected int endRange;
+    Random randGenerator;
+    
+    // constructor
+    public DataGenerator() {
+        dataSet = receiveData();
+        randGenerator = new Random(System.currentTimeMillis());
+    }
 
     public static void main(String[] args) {
-        timeEstimation();
-    }   // end of main
-    
-    private static void readFile() {
-        InputStream input = null;
         
-        try {
-            input = new BufferedInputStream(new FileInputStream("facebook_test.txt"));
-            byte[] buffer = new byte[8192];
-            
-            for (int length = 0; (length = input.read(buffer)) != -1;) {
-                System.out.write(buffer, 0, length);
-            }
-        } catch (IOException e) {
-            System.err.println(e.getLocalizedMessage());
-        }
-    }   // end of readFile
-    
-    private static void timeEstimation() {
+        DataGenerator DG = new DataGenerator();
         long startTime, endTime;
         double estimatedTime;
         
         startTime = System.nanoTime();
-        // test must be implemented inbetween startTime and endTime
-        readFile();
-        //
+        
+        for (int i = 0; i < DG.dataSet.length; i++) {
+            System.out.println(DG.dataSet[i]);
+        }
+//        System.out.println(DG.dataSet.length);
         endTime = System.nanoTime();
         
         estimatedTime = ((double) (endTime - startTime)) / Math.pow(10, 9);
         
         System.out.println("\ntime taken: " + estimatedTime + "sec");
+    }   // end of main
+    
+    public String[] receiveData() {
+        
+        String[] data = null;
+        int lineNum = 0;
+        BufferedReader BR = null;
+        String fileName = "facebook_test.txt";
+        try {
+            BR = new BufferedReader(new FileReader(fileName));     
+            String line;
+            data = new String[1507];
+            while ((line = BR.readLine()) != null) {
+                data[lineNum] = line;
+                
+                lineNum++;
+            }
+            if (BR != null) {
+                BR.close();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe.getLocalizedMessage());
+        }
+        
+        endRange = lineNum - 1;
+        return data;
     }
     
     
